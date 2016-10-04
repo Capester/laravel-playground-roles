119f3f00150dde249a99be9a48c89c4a9fb209f4
Gaby Lever <glever@eposbv.nl>
2016-10-04 10:52:16 +0200

    Extra lege regels
    Extra lege regels toegevoegd voor leesbaarheid

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,9 +23,13 @@ class AuthController extends Controller
     {
         $user = new User();
         $user->first_name = $request['first_name'];
+
         $user->last_name = $request['last_name'];
+
         $user->email = $request['email'];
+
         $user->password = $request['password'];
+        
         $user->save();
         $user->roles()->attach(Role::where('name', 'User')->first());
         Auth::login($user);
@@ -43,7 +47,7 @@ class AuthController extends Controller
     public function getLogout()
     {
         Auth::logout();
-        
+
         return redirect()->route('main');
     }
 }
